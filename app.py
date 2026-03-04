f34962a2eb6362d9b75eb35da0f28c6a21142e72
Natalia Stradaeva <natalistradaeva@gmail.com>
2026-03-04 21:05:05 +0100

    Refactor status page, initial app structure and stock page

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+olives_stock = 500  # Quantità di olive in stock
+
 @app.route('/')
 def home():
-    return '<h1>NS PureOil Production</h1><p>Sistema avviato!</p>'
+    return '<h1>NS PureOil Production</h1><p>Sistema avviato!</p><a href="/status">Controlla lo stock</a>'
+
+@app.route('/status')
+def status():
+    return f'''<h1>Stock di Olive</h1><p>Attualmente abbiamo {olives_stock} olive in stock.</p>
+    <a href="/">Torna alla home</a>'''
 
 if __name__ == '__main__':
     app.run(debug=True)
